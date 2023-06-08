e17dc7b60bda8470dbc6b6044b6cd8e4baa58994
Anton Borisov <anton.b@doit-intl.com>
2023-06-08 13:32:55 +0300

    change Updater parameters according to module docs

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import Updater, MessageHandler, filters, CallbackContext
 
 def forward_to_channel(update: Update, context: CallbackContext):
@@ -8,7 +8,8 @@ def forward_to_channel(update: Update, context: CallbackContext):
         context.bot.forward_message(chat_id=os.getenv('CHANNEL_ID'), from_chat_id=message.chat_id, message_id=message.message_id)
 
 def main():
-    updater = Updater(token=os.getenv('BOT_TOKEN'), use_context=True)
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    updater = Updater(bot=bot, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), forward_to_channel))
     updater.start_polling()
